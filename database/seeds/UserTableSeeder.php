b04481b93915f2c10d211dc28c1a9f52efa62829
Omar Monrroy <ogcm.kael@hotmail.com>
2019-04-14 23:30:46 -0500

    Usuarios test seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_administrativo = Role::where('name','Administrativo')->first();
+        $role_empleador = Role::where('name','Empleador')->first();
+        $role_empleado = Role::where('name', 'Empleado')->first();
+
+
+        $user = new User();
+        $user->name = 'Administrador';
+        $user->email = 'administrativo@example.com';
+        $user->password = bcrypt('secret');
+        $user->save();
+        $user->roles()->attach($role_administrativo);
+
+        $user = new User();
+        $user->name = 'Empleador';
+        $user->email = 'empleador@example.com';
+        $user->password = bcrypt('secret');
+        $user->save();
+        $user->roles()->attach($role_empleador);
+
+        $user = new User();
+        $user->name = 'Empleado';
+        $user->email = 'empleado@example.com';
+        $user->password = bcrypt('secret');
+        $user->save();
+        $user->roles()->attach($role_empleado);
+    }
+}
