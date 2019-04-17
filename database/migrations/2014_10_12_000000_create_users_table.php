1159d3e95b189e9563930970611340eb41fcbd2a
Omar Monrroy <ogcm.kael@hotmail.com>
2019-04-17 05:29:03 -0500

    Se añadieron los campos de apellido paterno y materno a la tabla usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name',50);
+            $table->string('lastname', 100);
+            $table->string('motherLastname', 100);
             $table->string('email',50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
