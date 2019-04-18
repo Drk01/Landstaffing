949e9014e255c94a4f1145ff826fe993bfef8fd6
Omar <ogcm.kael@hotmail.com>
2019-04-18 07:06:18 -0500

    Asignando posibilidad de llenado en masa, método user y BUGFIX

--- app/Address.php
+++ app/Address.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
+    protected $guarded = array();
+
     public function country(){
-        $this->belongsToMany('App\Country');
+       return $this->belongsToMany('App\Country')->withTimestamps();
+    }
+
+    public function user(){
+       return $this->belongsTo('App\User')->withTimestamps();
     }
 }
