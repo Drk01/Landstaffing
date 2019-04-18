f5143e761645a20f9c021e80ffc9c08bf77d0a54
Omar <ogcm.kael@hotmail.com>
2019-04-18 07:06:45 -0500

    BUGFIX

--- app/Country.php
+++ app/Country.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     public function address(){
-        $this->belongsToMany('App\Address');
+       return $this->belongsToMany('App\Address');
     }
 }
