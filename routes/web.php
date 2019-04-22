eb5998aa049b9029b05768903c8a70633dcdcff4
Omar Monrroy <ogcm.kael@hotmail.com>
2019-04-21 21:23:45 -0500

    Añadida la ruta "/my-account"
    Ruta que enlaza a al función HomeController en el método account

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/panel', 'HomeController@index')->name('home');
+
+Route::get('/my-account', 'HomeController@account')->name('account');
