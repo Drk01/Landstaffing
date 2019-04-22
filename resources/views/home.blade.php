133bab3671eebc94d40246b7939ba7c8e54ba584
Omar Monrroy <ogcm.kael@hotmail.com>
2019-04-21 21:32:37 -0500

    Modal
    -Se añadió la estructura del modal de inicio a todos los usuarios recién registrados.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="right_col" role="main">
-  <div class="">
+
     <div class="page-title">
       <div class="title_left">
         <h3>Plain Page</h3>
@@ -19,7 +18,7 @@
 
     <div class="clearfix"></div>
 
-    <div class="row"> 
+    <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
@@ -32,6 +31,34 @@
         </div>
       </div>
     </div>
-  </div>
-</div>
+
+<script type="text/javascript">
+window.onload= () => {
+  document.getElementById('open_modal').click();
+}
+</script>
+    <button id="open_modal" style="display: none" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
+
+    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+            </button>
+            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          </div>
+          <div class="modal-body">
+            <h4>Text in a modal</h4>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
 @endsection
