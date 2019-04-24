f91cb8b5f7541a8b86ed9d440e4006afa05b27cd
Omar Monrroy <ogcm.kael@hotmail.com>
2019-04-24 18:37:20 -0500

    Rellenando todos los datos de un empleador

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\User;
 use App\Role;
+use App\Address;
+use App\Country;
 
 class UserTableSeeder extends Seeder
 {
@@ -16,6 +18,7 @@ class UserTableSeeder extends Seeder
         $role_administrativo = Role::where('name','Administrativo')->first();
         $role_empleador = Role::where('name','Empleador')->first();
         $role_empleado = Role::where('name', 'Empleado')->first();
+        $Pais = Country::where('name','México')->first();
 
 
         $user = new User();
@@ -33,8 +36,22 @@ class UserTableSeeder extends Seeder
         $user->password = bcrypt('secret');
         $user->lastname = 'example';
         $user->motherLastname = 'example';
+        $user->empresa = 'Empresa de ejemplo';
+        $user->foto = 'img.png';
         $user->save();
         $user->roles()->attach($role_empleador);
+        $user->phone()->create([
+            'phone' => '1234567890'
+        ]);
+        $user->address()->create([
+            'estado' => 'Estado',
+            'ciudad' => 'Ciudad',
+            'calle' => 'Calle',
+            'extNumber' => '10',
+            'reference' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsam incidunt eligendi nihil assumenda id voluptas natus nobis esse! Ad distinctio iusto beatae modi fugiat corporis ea, deserunt voluptas voluptate.'
+        ]);
+        $Address = Address::all()->last();
+        $Address->country()->attach($Pais);
 
         $user = new User();
         $user->name = 'Empleado';
