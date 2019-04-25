54c0bb780e8d62be05ae586c068fe8dde12ee13e
Omar Monrroy <ogcm.kael@hotmail.com>
2019-04-25 03:32:18 -0500

    Sección de "Mis datos" terminada
    Se programó la función que guarda los datos del usuario y se realizaron reparaciones en la vista.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 use App\Phone;
 use App\Country;
+use App\Address;
 
 class HomeController extends Controller
 {
@@ -36,6 +37,56 @@ class HomeController extends Controller
     }
 
     public function saveData(Request $request){
+        $Validator = $request->validate([
+            'name' => 'string|required',
+            'lastname' => 'string|required',
+            'motherLastname' => 'string|required',
+            'company' => 'string|required|unique:users,empresa',
+            'email' => 'required|email',
+            'tel' => 'required|numeric',
+            'state' => 'required|string',
+            'city' => 'required|string',
+            'street' => 'required|string',
+            'extNumber' => 'required|numeric|min:1',
+            'references' => 'string',
+            'profile-picture' => 'image',
+            'country' => 'required|exists:countries,id'
+        ]);
 
+        if ($request->hasFile('profile-picture')) {
+            $image = $request['profile-picture'];
+            $image->move(public_path(). '/storage',auth()->user()->id.'.'.$request['profile-picture']->getClientOriginalExtension());
+        }
+
+        $user = User::where('id',auth()->user()->id)->first();
+        $user->name = $request->name;
+        $user->lastname = $request->lastname;
+        $user->motherLastname = $request->motherLastname;
+        $user->email = $request->email;
+        $user->empresa = $request->company;
+        if ($request->hasFile('profile-picture')) {
+            $user->foto = auth()->user()->id.'.'.$request->file('profile-picture')->getClientOriginalExtension();
+        }
+        $user->save();
+
+        $Address = Address::updateOrCreate(
+            ['user_id' => auth()->user()->id],
+            [
+                'estado' => $request->state,
+                'ciudad' => $request->city,
+                'calle' => $request->street,
+                'reference' => $request->references,
+                'extNumber' => $request->extNumber
+            ]
+        );
+        $Address->country()->detach();
+        $Address->country()->attach($request->country);
+
+        $phone = Phone::updateOrCreate(
+            ['user_id' => auth()->user()->id],
+            ['phone' => $request->tel]
+        );
+
+        return redirect(route('account'));
     }
 }
--- resources/views/myaccount/myaccount.blade.php
+++ resources/views/myaccount/myaccount.blade.php
@@ -22,19 +22,19 @@ Mis datos
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="lastname">Apellido paterno: </label>
-                            <input required type="text" class="form-control" id="lastname">
+                            <input required type="text" name="lastname" class="form-control" id="lastname">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="motherLastname">Apellido materno: </label>
-                            <input required type="text" class="form-control" id="motherLastname">
+                            <input required type="text" name="motherLastname" class="form-control" id="motherLastname">
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="profile-picture">Foto de perfil: </label>
-                    <input required type="file" name="profile-picture" class="form-control">
+                    <input type="file" name="profile-picture" class="form-control" accept="image/*" />
                 </div>
                 <div class="form-group">
                     <label for="company">Nombre de la empresa: </label>
@@ -43,7 +43,7 @@ Mis datos
                 <hr>
                 <div class="form-group">
                     <label for="email">Correo electrónico: </label>
-                    <input required type="email" class="form-control" id="email" accept="image/*" />
+                    <input required type="email" class="form-control" id="email" name="email">
                 </div>
                 <div class="row">
                     <div class="col-md-6">
@@ -62,7 +62,7 @@ Mis datos
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="phone">Teléfono:</label>
-                            <input required type="tel" class="form-control" id="phone">
+                            <input required type="tel" class="form-control" id="phone" name="tel" maxlength="10">
                         </div>
                     </div>
                 </div>
@@ -70,13 +70,13 @@ Mis datos
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="state">Estado: </label>
-                            <input required type="text" class="form-control" id="state">
+                            <input required type="text" class="form-control" id="state" name="state">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="city">Ciudad: </label>
-                            <input required type="text" class="form-control" id="city">
+                            <input required type="text" class="form-control" id="city" name="city">
                         </div>
                     </div>
                 </div>
@@ -84,13 +84,13 @@ Mis datos
                     <div class="col-md-8">
                         <div class="form-group">
                             <label for="street">Calle: </label>
-                            <input required type="text" class="form-control" id="street">
+                            <input required type="text" class="form-control" id="street" name="street">
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="extN">N° Externo</label>
-                            <input required type="number" class="form-control" id="extN">
+                            <input required type="number" class="form-control" id="extN" name="extNumber" minlength="1">
                         </div>
                     </div>
                 </div>
@@ -112,7 +112,7 @@ Mis datos
             <div class="panel-body">
                 <div class="row">
                     <div class="col-md-3">
-                        <img src="{{ asset('Panel/images/img.jpg') }}" alt="Foto de perfíl" width="100px">
+                        <img src="/storage/{{ auth()->user()->foto }}" alt="Foto de perfíl" width="100px">
                     </div>
                     <div class="col-md-9">
                         <h4><strong>Nombre: </strong> {{ auth()->user()->name }} </h4>
