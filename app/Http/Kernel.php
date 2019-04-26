3bf1fc236f25c20b90d37cfa738231f6d9b378e3
Omar <ogcm.kael@xn--otmail-o7a.com>
2019-04-26 12:43:07 -0500

    Registrando middleware "Empleador en el kernel"

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'Empleador' => \App\Http\Middleware\Empleador::class,
     ];
 
     /**
