f06494d3d7f92190ded446366ba54341faf0ebae
Omar Monrroy <ogcm.kael@xn--otmail-o7a.com>
2019-04-30 06:53:08 -0500

    Migración de la tabla "Ordenes"

---
+++ database/migrations/2019_04_27_034915_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name')->nullable();
+            $table->longText('description')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->dateTime('startAt');
+            $table->dateTime('endAt')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
