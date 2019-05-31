5579988ca442de4e1ca7932d8557a646a3d021c2
Omar <ogcm.kael@hotmail.com>
2019-05-30 22:32:28 -0500

    Declaración del CStatusTableSeeder
    Declaración del seeder de los status de los curriculum.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CountryTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(AbilitiesTableSeeder::class);
+        $this->call(CStatusTableSeeder::class);
     }
 }
